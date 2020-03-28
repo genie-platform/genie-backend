647d3fdffea49a185213178499936cfe20c5ac8d
Leon Prouger <leonprou@gmail.com>
2020-03-28 16:47:19 +0300

    fix error in models

--- src/models/Game.js
+++ src/models/Game.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose
 
 const GameSchema = new Schema({
   name: { type: String, required: [true, "can't be blank"] },
-  accountAddress: { type: String, required: [true, "can't be blank"] }
+  accountAddress: { type: String, required: [true, "can't be blank"] },
+  fund: { type: String, default: '0' }
 })
 
 const Game = mongoose.model('Game', GameSchema)
