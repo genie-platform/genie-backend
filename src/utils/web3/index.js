7de5267c3b7419372c24f0551c5b716572543260
Leon Prouger <leonprou@gmail.com>
2020-04-15 14:55:19 +0300

    delete unneeded code

--- src/utils/web3/index.js
+++ src/utils/web3/index.js
@@ -1,6 +1,5 @@
 const Web3 = require('web3')
 const ethUtils = require('ethereumjs-util')
-const ethers = require('ethers')
 const config = require('config')
 const { fromMasterSeed } = require('ethereumjs-wallet/hdkey')
 const { inspect } = require('util')
@@ -90,13 +89,12 @@ const send = async ({ web3, address }, method, options, handlers) => {
     })
 
     try {
-      debugger
       const receipt = await promise
       console.log(`method ${methodName} succeeded in tx ${receipt.transactionHash}`)
       return { receipt }
     } catch (error) {
       console.error(error)
-      debugger
+
       if (error.receipt) {
         return error
       }
@@ -108,11 +106,6 @@ const send = async ({ web3, address }, method, options, handlers) => {
         account.nonce = nonce
       }
 
-      if (error.receipt) {
-        await updateNonce()
-        account.save()
-        // throw error
-      }
 
       const errorHandlers = {
         [TRANSACTION_HASH_IMPORTED]: async () => {
@@ -169,60 +162,11 @@ const getPrivateKey = (account) => {
   return ethUtils.addHexPrefix(ethUtils.bufferToHex(derivedWallet.getPrivateKey()))
 }
 
-const toBufferStripPrefix = (str) => Buffer.from(ethUtils.stripHexPrefix(str), 'hex')
-
-const generateSignature = async (method, methodArguments, privateKey) => {
-  const msg = await method(...methodArguments).call()
-  const vrs = ethUtils.ecsign(toBufferStripPrefix(msg), toBufferStripPrefix(privateKey))
-  return ethUtils.toRpcSig(vrs.v, vrs.r, vrs.s)
-}
-
-const sha3 = (input) => {
-  if (ethers.utils.isHexString(input)) {
-    return ethers.utils.keccak256(input)
-  }
-  return ethers.utils.keccak256(ethers.utils.toUtf8Bytes(input))
-}
-
-const signMultiSigHash = (walletAddr, destinationAddr, value, data, nonce) => {
-  let input = '0x' + [
-    '0x19',
-    '0x00',
-    walletAddr,
-    destinationAddr,
-    ethers.utils.hexZeroPad(ethers.utils.hexlify(value), 32),
-    data,
-    ethers.utils.hexZeroPad(ethers.utils.hexlify(nonce), 32)
-  ].map(hex => hex.slice(2)).join('')
-
-  return sha3(input)
-}
-
-const signMultiSig = async (web3, account, multiSigContract, contractAddress, data) => {
-  // Get the nonce
-  const nonce = (await multiSigContract.methods.nonce().call()).toNumber()
-
-  // Get the sign Hash
-  let hash = signMultiSigHash(multiSigContract.address, contractAddress, 0, data, nonce)
-
-  // Get the off chain signature
-  const signHashBuffer = Buffer.from(hash.slice(2), 'hex')
-  const signature = web3.eth.accounts.sign(signHashBuffer, getPrivateKey(account))
-
-  return signature.signature
-}
-
-const generateSalt = () => {
-  return ethers.utils.bigNumberify(ethers.utils.randomBytes(32)).toHexString()
-}
 
 module.exports = {
   createWeb3,
-  generateSignature,
-  signMultiSig,
   createContract,
   createMethod,
   send,
-  createNetwork,
-  generateSalt
+  createNetwork
 }
